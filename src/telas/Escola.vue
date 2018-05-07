ed5d49cd0fa7a6c18665a20b844c68faf994509c
ArnaldoAF <arnaldo_assisferreira@outlook.com>
2018-05-06 23:15:12 -0300

    CRUD Escola - Completo

--- src/telas/Escola.vue
+++ src/telas/Escola.vue
@@ -1,8 +1,21 @@
 <template>
     <div>
         <h1>Escolas</h1>
-        <ul class="collection">
-            <li v-for="escola in escolas" class="collection-item">{{escola.nome}}</li>
+        <ul class="collapsible">
+            <li v-for="escola in escolasArray" class="collection-item">
+                <div class="collapsible-header">
+                    {{escola.nome}}
+                    <span class="badge">
+                        <a class="btn-floating  btn-small modal-trigger" href="#modal1" @click="EditarEscola(escola)" ><i class="material-icons">edit</i></a>
+                        <a class="btn-floating  btn-small" @click="ExcluirEscola(escola)"><i class="material-icons">delete</i></a>
+                    </span>
+                </div>
+                <div class="collapsible-body">
+                    <a class="btn-floating  btn-small" @click="ExcluirEscola(escola)"><i class="material-icons">delete</i></a>
+                    
+                    {{escola.id}}
+                </div>
+            </li>
         </ul>
         
 
@@ -22,8 +35,10 @@
         </div>
         </div>
         <div class="modal-footer">
-            <button class="btn modal-action modal-close red" @click="nomeEscola=''">CANCELAR</button>
-            <button class="btn modal-action modal-close green" @click="test()">INCLUIR</button>
+            <button class="btn modal-action modal-close red" @click="nomeEscola=''; EditingEscola=null">CANCELAR</button>
+            <button class="btn modal-action modal-close green" v-if="EditingEscola===null" @click="InserirEscola()">INCLUIR</button>
+            <button class="btn modal-action modal-close green" v-else @click="UpdateEscola()">EDITAR</button>
+            
         </div>
         </div>
 
@@ -38,7 +53,7 @@
 
     export default {
         firebase: {
-            escolas: Database.ref('escolas')
+            //escolas: Database.ref('escolas')
         },
         mounted: function () {
             // Jquery para o modal
@@ -49,12 +64,51 @@
                 $(document).on('shown.bs.modal', function (e) {
                 $('[autofocus]', e.target).focus();
                 });
+                $('.collapsible').collapsible();
             });
         },
         data() {
             return {
-                nomeEscola:''
+                escolasArray:[],
+                nomeEscola:'',
+                escolas: Database.ref('escolas'),
+                EditingEscola:null
             };
+        },
+        methods: {
+            InserirEscola () {
+                Database.ref('escolas').push({nome: this.nomeEscola});
+                this.nomeEscola='';
+            },
+            ExcluirEscola (escola) {
+                Database.ref('escolas').child(escola.id).remove();
+
+            },
+            EditarEscola (escola) {
+                this.EditingEscola = escola;
+                this.nomeEscola = escola.nome;
+            },
+            UpdateEscola () {
+                Database.ref('escolas').child(this.EditingEscola.id).update({nome: this.nomeEscola});
+                this.nomeEscola=''; 
+                this.EditingEscola=null;
+            }
+        },
+        created () {
+            Database.ref('escolas').on('child_added', snapshot => this.escolasArray.push({...snapshot.val(), id: snapshot.key}))
+            Database.ref('escolas').on('child_removed', snapshot => {
+                const EscolaRemovida = this.escolasArray.find(escola => escola.id === snapshot.key)
+                const index = this.escolasArray.indexOf(EscolaRemovida)
+                this.escolasArray.splice(index, 1);
+            })
+            Database.ref('escolas').on('child_changed', snapshot => {
+                const EscolaEditada = this.escolasArray.find(escola => escola.id === snapshot.key)
+                EscolaEditada.nome = snapshot.val().nome
+                //const index = this.escolasArray.indexOf(EscolaEditada)
+                //this.escolasArray.splice(index, 1,"snapshot.val().nome");
+            })
+            //Database.ref('escolas').on('value', snapshot => console.log(snapshot.val()));
+            
         }
     };
 </script>
